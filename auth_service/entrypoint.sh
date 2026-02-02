59983c0401b047fd179f3b81223a5fff2edb7e26
Vitor <marthendalnunes@gmail.com>
2026-02-02 19:04:11 -0300

    fix: migrate path

--- auth_service/entrypoint.sh
+++ auth_service/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running migrations..."
-pnpm dlx @better-auth/cli@1.3.4 migrate --config /src/server.ts --yes
+pnpm dlx @better-auth/cli@1.3.4 migrate --config src/server.ts --yes
 
 echo "Starting app..."
 exec pnpm dev --host "$HOST"
