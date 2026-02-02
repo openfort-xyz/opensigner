5036e46745aa6f935b73ed4c27bd109d00ad0bc8
Vitor <marthendalnunes@gmail.com>
2026-02-02 18:57:13 -0300

    fix: trust origin

--- auth_service/entrypoint.sh
+++ auth_service/entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running migrations..."
 pnpm dlx @better-auth/cli@1.3.4 migrate --config /src/server.ts --yes
 
 echo "Starting app..."
-exec pnpm dev -- --host "$HOST"
+exec pnpm dev --host "$HOST"
